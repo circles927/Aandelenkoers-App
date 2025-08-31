125c13faeaeaeeeb276e648b8b172ace76b5920e
circles927 <niels.magendans@gmail.com>
2025-08-31 18:45:30 +0200

    added functionality, now you can buy and sell, and it affects overal budget

--- tkinterapp2.0.py
+++ tkinterapp2.0.py
@@ -11,7 +11,17 @@ from tkinter import *
 
 window = Tk()
 window.title("Plotting the plot!")
-window.geometry("1000x950") 
+window.geometry("1200x1000") 
+
+budget = 200000
+
+totalAEX = 0
+totalLON = 0
+
+unitsAEX = 0
+unitsLON = 0
+
+pricing = 0
 
 def on_closing():
     try:
@@ -48,6 +58,7 @@ def displayplot():
     canvas.get_tk_widget().pack(side="top")
 
 def fetchdata():
+    global pricing
     stockSelected = stockselect.get()
     dateSelected = f"{dateSelectYear.get()}-{dateSelectMonth.get()}-{dateSelectDay.get()}"
 
@@ -57,7 +68,39 @@ def fetchdata():
         labelData.config(text="no data found")
     else:
         # dataFetched is a scalar close value; display it
-        labelData.config(text=str(dataFetched))
+        pricing = dataFetched
+        updateNetWorth()
+        labelData.config(text=f"Current Pricing: {pricing:.2f}")
+
+def updateNetWorth():
+    global budget, totalAEX, totalLON, unitsAEX, unitsLON, pricing
+    
+    totalAEX = unitsAEX * pricing
+    totalLON = unitsLON * pricing
+    labelTotalAEX.config(text=f"Total AEX: {totalAEX:.2f}")
+    labelTotalLON.config(text=f"Total LON: {totalLON:.2f}")
+
+def calculateResults(order):
+    global budget, totalAEX, totalLON, unitsAEX, unitsLON, pricing
+    orderStr = str(order)
+    if stockselect.get() == "AEX":
+        totalBuy = int(orderStr) * pricing
+        totalAEX += totalBuy
+        unitsAEX += int(orderStr)
+        budget -= totalBuy
+        labelbalance.config(text=f"Budget: {budget:.2f}")
+        labelTotalAEX.config(text=f"Total AEX: {totalAEX:.2f}")
+        labelUnitsAEX.config(text=f"Units AEX: {unitsAEX}")
+    elif stockselect.get() == "London_Exchange":
+        totalBuy = int(orderStr) * pricing
+        totalLON += totalBuy
+        unitsLON += int(orderStr)
+        budget -= totalBuy
+        labelbalance.config(text=f"Budget: {budget:.2f}")
+        labelTotalLON.config(text=f"Total LON: {totalLON:.2f}")
+        labelUnitsLON.config(text=f"Units LON: {unitsLON}")
+    else: 
+        print("no stock selected")
 
 
 bottomframe = Frame(window)
@@ -96,18 +139,45 @@ dateSelectDay.place(x=340, y=675)
 selectionMadeButton = Button(window, text="select date", command=fetchdata)
 selectionMadeButton.place(x=380, y=675)
 
-# create a persistent label to show fetched data (initially empty)
-labelData = Label(window, text="", font=16)
-labelData.place(x=600, y=575)
+# ---
 
-labelbalance = Label(window, text="20.000", font=38)
+labelbalance = Label(window, text=f"Budget: {budget:.2f}", font=38)
 labelbalance.place(x=250, y=725)
 
-buttonbuy = Button(window, text="buy order")
-buttonbuy.place(x=250, y=750)
+buttonbuy1 = Button(window, text="buy order 1", width=15, command=lambda: calculateResults(+1))
+buttonbuy1.place(x=250, y=760)
 
-buttonsell = Button(window, text="sell order")
-buttonsell.place(x=320, y=750)
+buttonsell1 = Button(window, text="sell order 1", width=15, command=lambda: calculateResults(-1))
+buttonsell1.place(x=375, y=760)
+
+buttonbuy10 = Button(window, text="buy order 10", width=15, command=lambda: calculateResults(+10))
+buttonbuy10.place(x=250, y=795)
+
+buttonsell10 = Button(window, text="sell order 10", width=15, command=lambda: calculateResults(-10))
+buttonsell10.place(x=375, y=795)
+
+buttonbuy200 = Button(window, text="buy order 200", width=15, command=lambda: calculateResults(+200))
+buttonbuy200.place(x=250, y=830)
+
+buttonsell200 = Button(window, text="sell order 200", width=15, command=lambda: calculateResults(-200))
+buttonsell200.place(x=375, y=830)
+
+# create a persistent label to show fetched data (initially empty)
+labelData = Label(window, text=f"Current Pricing: {pricing:.2f}", font=20)
+labelData.place(x=625, y=575)
+
+labelTotalAEX = Label(window, text=f"Total AEX: {totalAEX:.2f}", font=20)
+labelTotalAEX.place(x=600, y=635)
+
+labelUnitsAEX = Label(window, text=f"Units AEX: {unitsAEX:.2f}", font=20)
+labelUnitsAEX.place(x=800, y=635)
+
+labelTotalLON = Label(window, text=f"Total LON: {totalLON:.2f}", font=20)
+labelTotalLON.place(x=600, y=670)
+
+labelUnitsLON = Label(window, text=f"Units LON: {unitsLON:.2f}", font=20)
+labelUnitsLON.place(x=800, y=670)
+# ---
 
 # Add button to screen
 # setupgui()
